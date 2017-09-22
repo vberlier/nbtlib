392e00a6eb8b76b9dd64ecbb94193e6bbd365ea3
Valentin Berlier <berlier.v@gmail.com>
2017-09-22 16:06:38 +0200

    Add basic parsing tests for all tags

--- tests/test_tag.py
+++ tests/test_tag.py
@@ -32,6 +32,41 @@ tag_parsing_inputs = [
     (b'\x7F\xFF\xFF\xFF\xFF\xFF\xFF\xFF', Long(9223372036854775807)),
     (b'\x80\x00\x00\x00\x00\x00\x00\x00', Long(-9223372036854775808)),
 
+    # Float tag
+    (b'\x00\x00\x00\x00', Float(0)),
+    (b'\xbf\x80\x00\x00', Float(-1)),
+    (b'>\xff\x182',       Float(0.49823147058486938)),
+
+    # Double tag
+    (b'\x00\x00\x00\x00\x00\x00\x00\x00', Double(0)),
+    (b'\xbf\xf0\x00\x00\x00\x00\x00\x00', Double(-1)),
+    (b'?\xdf\x8fk\xbb\xffj^',             Double(0.49312871321823148)),
+
+    # ByteArray tag
+    (b'\x00\x00\x00\x00',             ByteArray([])),
+    (b'\x00\x00\x00\x01\xff',         ByteArray([-1])),
+    (b'\x00\x00\x00\x03\x01\x02\x03', ByteArray([1, 2, 3])),
+
+    # String tag
+    (b'\x00\x00',                         String('')),
+    (b'\x00\x0bhello world',              String('hello world')),
+    (b'\x00\x06\xc3\x85\xc3\x84\xc3\x96', String('ÅÄÖ')),
+
+    # List tag
+    (b'\x02\x00\x00\x00\x00',                           List[Short]([])),
+    (b'\x01\x00\x00\x00\x04\x05\xf7\x12\x40',           List[Byte]([Byte(5), Byte(-9), Byte(18), Byte(64)])),
+    (b'\x08\x00\x00\x00\x02\x00\x05hello\x00\x05world', List[String]([String('hello'), String('world')])),
+
+    # Compound tag
+    (b'\x00',                                   Compound({})),
+    (b'\x03\x00\x03foo\x00\x00\x00*\x00',       Compound({'foo': Int(42)})),
+    (b'\x01\x00\x01a\x00\x01\x00\x01b\x01\x00', Compound({'a': Byte(0), 'b': Byte(1)})),
+
+    # IntArray tag
+    (b'\x00\x00\x00\x00',                                                 IntArray([])),
+    (b'\x00\x00\x00\x01\xff\xff\xff\xff',                                 IntArray([-1])),
+    (b'\x00\x00\x00\x03\x00\x00\x00\x01\x00\x00\x00\x02\x00\x00\x00\x03', IntArray([1, 2, 3])),
+
 ]
 
 
