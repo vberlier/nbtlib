aeb144dd2f3a01a12592e071379e6ea89dc76c5d
Valentin Berlier <berlier.v@gmail.com>
2017-11-19 23:44:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,24 @@
 
 from setuptools import setup
-from os import path
-
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.rst')) as f:
-    long_description = f.read()
 
 
 setup(
     name='nbtlib',
-    version='0.1.6',
+    version='0.1.7',
     description='A python package to read and edit nbt data',
-    long_description=long_description,
+    long_description=open('README.rst').read(),
     url='https://github.com/vberlier/nbtlib',
     author='Valentin Berlier',
     author_email='berlier.v@mail.com',
     platforms=['any'],
     license='MIT',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     keywords='nbt schema minecraft package library parser reader module',
     packages=['nbtlib'],
