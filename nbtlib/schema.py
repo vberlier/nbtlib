958864116422edb8ab2a655c56b1c04a5d41850c
Valentin Berlier <berlier.v@gmail.com>
2017-11-19 15:57:20 +0100

    Rename _match_schema() to _cast() and fix bug in update() in CompoundSchema

--- nbtlib/schema.py
+++ nbtlib/schema.py
@@ -19,20 +19,20 @@ class CompoundSchema(Compound):
         super().__init__(*args, **kwargs)
 
         for key, value in self.items():
-            correct_value = self._match_schema(key, value)
+            correct_value = self._cast(key, value)
             if correct_value is not value:
                 super().__setitem__(key, correct_value)
 
     def __setitem__(self, key, value):
-        super().__setitem__(key, self._match_schema(key, value))
+        super().__setitem__(key, self._cast(key, value))
 
     def update(self, mapping, **kwargs):
         pairs = chain(mapping.items(), kwargs.items())
         super().update(
-            (key, self._match_schema(value)) for key, value in pairs
+            (key, self._cast(key, value)) for key, value in pairs
         )
 
-    def _match_schema(self, key, value):
+    def _cast(self, key, value):
         schema_type = self.schema.get(key, None)
         if schema_type and not isinstance(value, schema_type):
             return schema_type(value)
