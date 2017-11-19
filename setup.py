31ce584ef480b54504b66a0d0988fc736ef33e92
Valentin Berlier <berlier.v@gmail.com>
2017-11-19 19:02:35 +0100

    Fix long description

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 
 from setuptools import setup, find_packages
-from codecs import open
 from os import path
 
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst')) as f:
     long_description = f.read()
 
 
 setup(
     name='nbtlib',
-    version='0.1.2',
+    version='0.1.3',
     description='A python package to read and edit nbt data',
     long_description=long_description,
     url='https://github.com/vberlier/nbtlib',
