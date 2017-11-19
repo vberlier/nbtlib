66fa101e8973252f31efc49836f359dcbefc9ada
Valentin Berlier <berlier.v@gmail.com>
2017-11-19 16:36:36 +0100

    Add docstrings to schema.py

--- nbtlib/schema.py
+++ nbtlib/schema.py
@@ -1,17 +1,48 @@
 
-from itertools import chain
+"""This module defines tools for creating tag schemas.
 
-from .tag import Compound
+Exported items:
+    schema         -- Helper function to define compound schemas
+    CompoundSchema -- `Compound` subclass that enforces a tag schema
+"""
 
 
 __all__ = ['schema', 'CompoundSchema']
 
 
+from itertools import chain
+
+from .tag import Compound
+
+
 def schema(name, dct):
+    """Create a compound tag schema.
+
+    This function is a short convenience function that makes it easy to
+    subclass the base `CompoundSchema` class.
+
+    The `name` argument is the name of the class and `dct` should be a
+    dictionnary containing the actual schema. The schema should map keys
+    to tag types or other compound schemas.
+    """
     return type(name, (CompoundSchema,), {'__slots__': (), 'schema': dct})
 
 
 class CompoundSchema(Compound):
+    """Class that extends the base `Compound` tag by enforcing a schema.
+
+    Defining a custom schema is really useful if you're dealing with
+    recuring data structures. Subclassing the `CompoundSchema` class
+    with your own schema will save you some typing by casting all the
+    keys defined in the schema to the appropriate tag type.
+
+    The class inherits from `Compound` and will cast values to the
+    predefined tag types for all of the inherited mutating operations.
+
+    Class attributes:
+        schema -- Dictionnary mapping keys to tag types or other schemas
+    """
+
     __slots__ = ()
     schema = {}
 
