61c0e2eedcba4939f4b13272bb454294e4e9cbe9
Valentin Berlier <berlier.v@gmail.com>
2018-09-28 22:54:19 +0200

    Version 1.2.1

--- nbtlib/__init__.py
+++ nbtlib/__init__.py
@@ -3,4 +3,4 @@ from .nbt import *
 from .schema import *
 from .literal import *
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as requirements:
 
 setup(
     name='nbtlib',
-    version='1.2.0',
+    version='1.2.1',
     license='MIT',
     description='A python package to read and edit nbt data',
     long_description=long_description,
