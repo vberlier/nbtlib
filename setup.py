9876917f58f39f0e9fc9736ce8d56525052b3f60
Valentin Berlier <berlier.v@gmail.com>
2019-02-02 12:49:40 -0500

    Version 1.3.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as requirements:
 
 setup(
     name='nbtlib',
-    version='1.3.1',
+    version='1.3.2',
     license='MIT',
     description='A python package to read and edit nbt data',
     long_description=long_description,
