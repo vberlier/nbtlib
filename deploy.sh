59f978d2341fdae06043f8c8c08e54b340fd0143
Valentin Berlier <berlier.v@gmail.com>
2019-02-04 12:26:34 -0500

    Check if the package is already on PyPI before publishing

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/env bash
 
-poetry publish \
-  --username=$PYPI_USERNAME \
-  --password=$PYPI_PASSWORD \
-  --no-interaction
+published () {
+  local releases=$(curl -sSL "https://pypi.org/simple/${PWD##*/}/")
+
+  for build in $(ls dist); do
+    if ! echo $releases | grep -Fq "$build"; then
+      return 1
+    fi
+  done
+
+  return 0
+}
+
+deploy () {
+  local username=$1
+  local password=$2
+
+  poetry publish \
+    --username="$username" \
+    --password="$password" \
+    --no-interaction
+}
+
+if published; then
+  echo -e "\n$TRAVIS_TAG already published\n"
+else
+  echo -e "\nPublishing $TRAVIS_TAG\n"
+  deploy $PYPI_USERNAME $PYPI_PASSWORD
+fi
