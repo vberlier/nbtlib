eeccd7000090a6d54ab486af41f00a46ebe92c9b
Valentin Berlier <berlier.v@gmail.com>
2019-02-04 13:34:32 -0500

    Fix releases url

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@
 published () {
   echo -e "\nComparing built distributions with published releases"
 
-  local releases=$(curl -sSL "https://pypi.org/simple/${PWD##*/}/")
+  local releases=$(curl -sSL "https://pypi.org/simple/nbtlib/")
 
   for build in $(ls dist); do
     echo "Checking $build"
 
-    if ! echo $releases | grep -Fq "$build"; then
-      echo "Couldn't find build $build in the published releases"
+    if ! echo "$releases" | grep -Fq "$build"; then
+      echo -e "\n\033[0;31mCouldn't find build $build in the published releases\033[0m"
       return 1
     fi
   done
@@ -25,8 +25,8 @@ deploy () {
 }
 
 if published; then
-  echo -e "\n\033[1;36m$TRAVIS_TAG already published\033[0m"
+  echo -e "\n\033[1;36m$TRAVIS_TAG is already published\033[0m"
 else
   echo -e "\n\033[1;36mPublishing $TRAVIS_TAG\033[0m"
-  exit $(deploy $PYPI_USERNAME $PYPI_PASSWORD)
+  deploy $PYPI_USERNAME $PYPI_PASSWORD || exit 1
 fi
